ba7e3feddb1782a8331d1681bb9fd0c52b7ad186
savion <didiapraisechimeka@gmail.com>
2026-02-05 12:30:24 +0100

    i started implementing some logic

--- app.js
+++ app.js
@@ -16,29 +16,30 @@ async function getProduct() {
         const tablet = product.image.tablet;
         const wind = window.screen.width >= 600 ? tablet : mobile;
         const desktop = product.image.desktop;
+        const cartItems = 0;
         const elements = `
                             <div id="box" class="box">
                         <div>
                             <img
                           ]      src="${wind}" 
                             />
-                            <button class="cart" onclick="hide">
+                            <button class="cart" >
                                 <img
                                     src="assets/images/icon-add-to-cart.svg"
                                     alt="cart-icon"
                                 />
                                 Add to Cart
                             </button>
-                            <button class="order-qty">
-                                <div>
+                            <button class="order-qty edit-qty">
+                                <div class="inc">
                                     <img
                                         src="assets/images/icon-increment-quantity.svg"
                                         alt="add-item-to-cart-btn"
                                         id="add-item"
                                     />
                                 </div>
-                                <p id="number-of-items">0</p>
-                                <div>
+                                <p id="number-of-items" class="order-items">${cartItems}</p>
+                                <div class="dec">
                                     <img
                                         src="assets/images/icon-decrement-quantity.svg"
                                         alt="remove-item-from-cart-btn"
@@ -59,10 +60,36 @@ async function getProduct() {
         products.push(elements);
     });
     productContainer.innerHTML = products.join("");
+    const cartBtn = document.querySelectorAll(".cart");
+
+    cartBtn.forEach((cart) => {
+        cart.classList.add("under");
+        cart.addEventListener("click", () => {
+            cart.classList.remove("under");
+        });
+    });
+
+    const orderDec = document.querySelectorAll(".edit-qty .dec");
+    const orderTotal = document.querySelectorAll("#order-items");
+    const orderInc = document.querySelectorAll(".edit-qty .inc");
+
+    orderDec.forEach((order) => {
+        let qty = 0;
+        order.addEventListener("click", () => {
+            orderTotal.innerHTML = qty <= 0 ? 0 : qty--;
+            console.log(qty);
+            console.log(orderTotal);
+        });
+    });
+    orderInc.forEach((order) => {
+        let qty = 0;
+        order.addEventListener("click", () => {
+            qty++;
+            console.log(qty);
+            console.log(orderTotal);
+            orderTotal.innerText = qty;
+        });
+    });
 }
 
 getProduct();
-
-const hide = () => {
-    document.style.zIndex = "-9";
-};
